9a072d631085ecca583268a7691012e6f3e6b9d2
ljx213101212 <lijixiang.nus@gmail.com>
2021-12-07 08:36:42 +0800

    env: config basic environment

---
+++ src/pages/1.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+const TwoSum = () => (
+  <>
+    <div>01. Two Sum</div>
+    <div>static code</div>
+    <div>user input</div>
+    <div>algo visualization</div>
+  </>
+);
+
+export default TwoSum;
---
+++ src/pages/helloworld.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Router, Link, Location } from '@reach/router';
+const TwoSum = () => (
+  <>
+    <div>01. Two Sum</div>
+    <div>static code</div>
+    <div>user input</div>
+    <div>algo visualization</div>
+
+    <Link to="/">Back To DashBoard</Link>
+  </>
+);
+
+export default TwoSum;
